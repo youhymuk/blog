77bdd87e2436a4fe14ef3b2398fb46afc55f64be
Dmytro Yukhymuk <dyukhymuk@swivl.com>
2021-02-21 10:36:57 +0200

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HTMLPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: __dirname +  '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: __dirname + '/dist'
+  },
+  plugins: [
+    new HTMLPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+} 
