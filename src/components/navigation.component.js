c72c941b7af7766b6a7538e32911aa6efe01de10
Dmytro Yukhymuk <dyukhymuk@swivl.com>
2021-02-23 23:47:18 +0200

    Implements tabs

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,10 +3,16 @@ import {Component} from '../core/component'
 export class NavigationComponent extends Component {
   constructor(id) {
     super(id)
+
+    this.tabs = []
   }
 
   init() {
     this.$el.addEventListener('click', handleTabClick.bind(this))
+  }
+
+  registerTabs(tabs) {
+    this.tabs = tabs
   }
 }
 
@@ -15,5 +21,9 @@ function handleTabClick(e) {
   if(e.target.classList.contains('tab')) {
     Array.from(this.$el.querySelectorAll('.tab'), el => el.classList.remove('active'));
     e.target.classList.add('active')
+
+    const activeTab = this.tabs.find(tab => tab.name === e.target.dataset.name)
+    this.tabs.forEach(tab => tab.component.hide())
+    activeTab.component.show()
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import {HeaderComponent} from './components/header.component'
 import {NavigationComponent} from './components/navigation.component'
+import {CreateComponent} from './components/create.component'
+import {PostsComponent} from './components/posts.component'
+import {FavoriteComponent} from './components/favorite.component'
 
 new HeaderComponent('header')
-new NavigationComponent('navigation')
+const navigation = new NavigationComponent('navigation')
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
+const posts = new PostsComponent('posts')
+
+navigation.registerTabs([
+  {name: 'create', component: create},
+  {name: 'posts', component: posts},
+  {name: 'favorite', component: favorite}
+])
